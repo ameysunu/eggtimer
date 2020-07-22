b401fde0aae8940298f7c9114e9363bddcb8db76
Amey Viney <amey@Ameys-Mac.local>
2020-07-22 01:33:02 -0700

    audio added

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
     
@@ -40,8 +42,27 @@ class ViewController: UIViewController {
         else {
             timer.invalidate()
             titleLabel.text = "Done!"
+            playSound()
         }
     }
-  
+  func playSound() {
+      guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+      do {
+          try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+          try AVAudioSession.sharedInstance().setActive(true)
+
+         
+          player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+
+          guard let player = player else { return }
+
+          player.play()
+
+      } catch let error {
+          print(error.localizedDescription)
+      }
+  }
     
 }
